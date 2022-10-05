cd19fb4631d7bdf032f9426aec286eab6606aff5
jiho_kwon <imperiaiiist@naver.com>
2022-10-05 16:33:41 +0900

    노동 씬 구현, 광물 캐기 구현

---
+++ Assets/Script/LaborScene/ScvMove.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScvMove : MonoBehaviour
+{
+    [SerializeField] Transform baseTrf, mineralTrf;
+
+    Vector3 target;
+    bool isMindir = true;//지금 미네랄로 향하고 있는가?
+
+    public GameObject mineral_Dumy;//자기가 들고 있는 광물더미
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DirSet();
+    }
+
+    float moveSpeed = 3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += this.transform.forward * moveSpeed * Time.deltaTime;
+
+        if ((target - this.transform.position).magnitude < 1f)
+        {
+            isMindir = !isMindir;
+            DirSet();
+        }
+    }
+    void DirSet()
+    {
+        if (isMindir)
+        {
+            target = mineralTrf.position;
+            mineral_Dumy.SetActive(false);
+        }
+        else
+        {
+            target = baseTrf.position;
+            mineral_Dumy.SetActive(true);
+        }
+        this.transform.forward = target - this.transform.position;
+    }
+}
