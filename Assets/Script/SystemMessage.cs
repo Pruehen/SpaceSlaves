304f531eb0fd32a33a9796dc5a4fd50ea44a8da0
pagi0721 <selly7213@gmail.com>
2022-10-11 10:05:31 +0900

    UI_메시지 내려가게

--- Assets/Script/SystemMessage.cs
+++ Assets/Script/SystemMessage.cs
@@ -1,9 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SystemMessage : MonoBehaviour
 {
+    public ScrollRect scroll = null;
+    public TextMeshProUGUI logText = null;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        MessageDown();
+    }
+
+    void MessageDown()
+    {
+        scroll.verticalNormalizedPosition = 0.0f;
+    }
+
     public void MessageQ()
     {
 
