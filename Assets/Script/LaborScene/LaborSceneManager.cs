fdbee29d061e9d41928ad62c0d87e94746ab1239
jiho_kwon <imperiaiiist@naver.com>
2022-10-14 15:42:29 +0900

    편성 ui 개선

--- Assets/Script/LaborScene/LaborSceneManager.cs
+++ Assets/Script/LaborScene/LaborSceneManager.cs
@@ -17,22 +17,31 @@ public class LaborSceneManager : MonoBehaviour
     {
         for (int i = 0; i < 20; i++)
         {
-            shipNameTmp[i] = buildBtnTrf.GetChild(i).GetChild(0).GetComponent<TextMeshProUGUI>();//텍스트 배열에 객체 집어넣음.
-            shipQtyTmp[i] = buildBtnTrf.GetChild(i).GetChild(1).GetComponent<TextMeshProUGUI>();//위와 같음
-
             ShipInfoData data = FleetManager.instance.GetShipData(i);//i 함선 데이터 불러옴
+
+            shipNameTmp[i] = buildBtnTrf.GetChild(i).GetChild(0).GetComponent<TextMeshProUGUI>();//텍스트 배열에 객체 집어넣음.
+            shipQtyTmp[i] = buildBtnTrf.GetChild(i).GetChild(1).GetComponent<TextMeshProUGUI>();//위와 같음            
+
+            shipNameTmp[i].text = data.shipName + "Class " + data.shipClass;//함선의 이름과 함종을 불러와서 텍스트에 입력
+            shipQtyTmp[i].text = FleetManager.instance.GetFleetQtyData(i).ToString();//함선의 수량을 텍스트에 입력
+
+            shipNameTmp[i] = shipSelectBtnTrf.GetChild(i).GetChild(0).GetComponent<TextMeshProUGUI>();//텍스트 배열에 객체 집어넣음.
+            shipQtyTmp[i] = shipSelectBtnTrf.GetChild(i).GetChild(1).GetComponent<TextMeshProUGUI>();//위와 같음            
 
             shipNameTmp[i].text = data.shipName + "Class " + data.shipClass;//함선의 이름과 함종을 불러와서 텍스트에 입력
             shipQtyTmp[i].text = FleetManager.instance.GetFleetQtyData(i).ToString();//함선의 수량을 텍스트에 입력
         }
     }
 
-    public GameObject buildWdw;
     public Transform buildBtnTrf;//버튼 상위 트랜스폼. 버튼들의 자식 객체들에게 접근하기 위함.
+    public Transform shipSelectBtnTrf;
     TextMeshProUGUI[] shipNameTmp = new TextMeshProUGUI[20]; //각 함선별 지정된 이름
     TextMeshProUGUI[] shipQtyTmp = new TextMeshProUGUI[20]; //각 함선별 수량
 
+
+    public GameObject buildWdw;
     public GameObject fleetFormationWdw;
+    public GameObject shipSelectWdw;
 
     public void BuildWdwToggle(bool value)//함선 건조 버튼을 눌렀을 때 실행. bool이 true일 경우 창이 켜지고 false이면 창이 꺼짐
     {
@@ -50,6 +59,11 @@ public class LaborSceneManager : MonoBehaviour
     public void ffWdwToggle(bool value)
     {
         fleetFormationWdw.SetActive(value);
+    }
+
+    public void ShipSelectWdwToggle(bool value)
+    {
+        shipSelectWdw.SetActive(value);
     }
 
     public void ShipAdd(int index)//함선 생산 버튼을 눌렀을 때 실행. 누른 버튼에 따라서 무슨 함선을 만들지 고름.
