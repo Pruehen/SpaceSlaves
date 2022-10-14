2c4d5e505d7c40f37d9267e68621ccfa863444cc
pagi0721 <selly7213@gmail.com>
2022-10-14 16:26:55 +0900

    카메라 이동 수정

--- Assets/Script/BattleScene/CameraControl.cs
+++ Assets/Script/BattleScene/CameraControl.cs
@@ -5,82 +5,105 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     bool isClicked = false;
+    bool IsRClicked = false;
     Vector2 clickPoint;
+    Vector2 UpPoint;
+    Vector2 MovePos;
     float dragSpeed = 15f;
-    float WheelSpeed = 10f;
+    float WheelSpeed = 4f;
+    public GameObject Cam;
 
     private void Update()
     {
-        Vector3 position = Camera.main.ScreenToViewportPoint((Vector2)Input.mousePosition - clickPoint);
+        Vector3 CamPos = Cam.transform.position;
 
         ClickCheck();
+        DragPos(clickPoint);
+        CamMove(MovePos);
 
-        if(isClicked == true)
-        {
-            CamMove(position);
-        }
+        ZoomIN_Ont(CamPos);
+    }
 
-        ZoomIN_Ont(position);
+    void DragPos(Vector2 Pos)
+    {
+        Vector2 Value = Pos - UpPoint;
+        Value = Value.normalized;
+
+        MovePos = 
+            new Vector2(Value.x * 100 * Time.deltaTime, 
+            Value.y * 100 * Time.deltaTime);
     }
 
     void ZoomIN_Ont(Vector3 Pos)
     {
-        if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        if(Input.GetAxis("Mouse ScrollWheel") < 0)//ÈÙ³»¸±‹š ÁÜÀÎ
         {
             //Camera.main.fieldOfView += WheelSpeed; // fov·Î ÀÌµ¿
-            Pos.z = Pos.y;
-            Pos.y = 0f;
 
-            Vector3 move = Pos * (Time.deltaTime * dragSpeed);
+            Vector3 move = Pos * (Time.deltaTime * WheelSpeed);
 
-            float x = transform.position.x;
+            float z = transform.position.z;
+            //float z = transform.position.z;
 
             transform.Translate(move);
-            transform.transform.position = new Vector3(x, transform.position.y, transform.position.z);
+            transform.transform.position = 
+                new Vector3(transform.position.x, transform.position.y, z);
         }
-        else if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        else if(Input.GetAxis("Mouse ScrollWheel") > 0)//ÈÙ´ç±æ‹š ÁÜ¾Æ¿ô
         {
             //Camera.main.fieldOfView -= WheelSpeed; //fov·Î ÀÌµ¿
-            Pos.z = Pos.y;
-            Pos.y = 0f;
 
-            Vector3 move = -Pos * (Time.deltaTime * dragSpeed);
+            Vector3 move = -Pos * (Time.deltaTime * WheelSpeed);
 
-            float x = transform.position.x;
             float z = transform.position.z;
+            //float z = transform.position.z;
 
             transform.Translate(move);
-            transform.transform.position = new Vector3(x, transform.position.y, transform.position.z);
+            transform.transform.position = 
+                new Vector3(transform.position.x, transform.position.y, z);
+
+            Debug.Log(transform.position.y);
         }
     }
 
     void CamMove(Vector3 Pos)
     {
-        Pos.z = Pos.y;
-        Pos.y = 0f;
+        if (isClicked == true)
+        {
+            Pos.z = Pos.y;
+            Pos.y = 0f;
 
-        Vector3 move = -Pos * (Time.deltaTime * dragSpeed);
+            Vector3 move = Pos * (Time.deltaTime * dragSpeed);
 
-        float y = transform.position.y;
+            float y = transform.position.y;
 
-        transform.Translate(move);
-        transform.transform.position = new Vector3(transform.position.x, y, transform.position.z);
+            transform.Translate(move);
+            transform.transform.position = 
+                new Vector3(transform.position.x, y, transform.position.z);
+        }
     }
 
     void ClickCheck()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            isClicked = true;
             clickPoint = Input.mousePosition;
         }
-
-        if (Input.GetMouseButton(0))
-        {
-            isClicked = true;
-        }
-        else
+        else if(Input.GetMouseButtonUp(0))
         {
             isClicked = false;
+            UpPoint = Input.mousePosition;
+        }
+        else if(Input.GetMouseButtonDown(1))
+        {
+            IsRClicked = true;
+            clickPoint = Input.mousePosition;
+        }
+        else if(Input.GetMouseButtonUp(1))
+        {
+            IsRClicked = false;
+            UpPoint = Input.mousePosition;
         }
     }
 }
