be01d710ee4e0bc63864092f57e0bd6cb20be10e
jiho_kwon <imperiaiiist@naver.com>
2022-10-28 12:25:04 +0900

    전투씬에 행성 추가, 행성 거리별 조명 개선

---
+++ Assets/Script/BattleScene/AstroBuilder.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroBuilder : MonoBehaviour
+{
+    public List<GameObject> planetSystems;
+    public Light sunLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planetSystems[StageManager.instance.selectedStage].SetActive(true);
+
+        switch (StageManager.instance.selectedStage)
+        {
+            case 0:
+                sunLight.intensity = 1.7f;
+                break;
+            case 1:
+                sunLight.intensity = 1.8f;
+                break;
+            case 2:
+                sunLight.intensity = 2f;
+                break;
+            case 3:
+                sunLight.intensity = 1.5f;
+                break;
+            case 4:
+                sunLight.intensity = 1.2f;
+                break;
+            case 5:
+                sunLight.intensity = 1f;
+                break;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
