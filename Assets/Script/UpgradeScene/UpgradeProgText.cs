1f785709148f84410068ada8a93639d9dd001b8b
jiho_kwon <imperiaiiist@naver.com>
2022-10-28 14:15:28 +0900

    업그레이드ui 가격추가

--- Assets/Script/UpgradeScene/UpgradeProgText.cs
+++ Assets/Script/UpgradeScene/UpgradeProgText.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 public class UpgradeProgText : MonoBehaviour
 {
     public UPGRADE_TYPE type;
-    public TMPro.TMP_Text txt;
+    public TMPro.TMP_Text infoTxt;
+    public TMPro.TMP_Text priceTxt;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,17 +24,18 @@ public class UpgradeProgText : MonoBehaviour
         var total = UpgradeManager.GetTotalActiveVal(type);
         var lv = UpgradeManager.instance.GetBestUpgradeId(type) % 1000;
 
+        priceTxt.text = UpgradeStaticManager.instance.GetCost(UpgradeManager.instance.GetBestUpgradeId(type)).ToString();
         // 함대 레벨은 다르게 처리
         switch (type)
         {
             case UPGRADE_TYPE.NONE:
-                txt.text = "WTF is this type of value doing here?";
+                infoTxt.text = "WTF is this type of value doing here?";
                 break;
             case UPGRADE_TYPE.FLEET:
-                txt.text = string.Format("{0} lv", lv + 1);
+                infoTxt.text = string.Format("{0} lv", lv + 1);
                 break;
             default:
-                txt.text = string.Format("{0} lv / 총합 {1} + ", lv, total);
+                infoTxt.text = string.Format("{0} lv / 총합 {1} + ", lv, total);
                 break;
         }
 
