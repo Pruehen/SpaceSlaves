75962d7becc9f03c303d5ebfed0633f304454cbd
WikiMuncher <mergo4848@gmail.com>
2022-10-28 14:54:40 +0900

    광물 수집기 - 버튼 기능 연결

---
+++ Assets/Script/UI/TimerUI.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerUI : MonoBehaviour
+{
+    public TMP_Text timerTxt;
+
+    // Update is called once per frame
+    public void UpdateTimer(long ticks)
+    {
+        TimeSpan timesp = new TimeSpan(ticks);
+        timerTxt.text = string.Format("{0:D2}:{1:D2}:{2:D2}", timesp.Hours,timesp.Minutes,timesp.Seconds);
+    }
+    public void SetAvail(bool isAvaile)
+    {
+        timerTxt.color = isAvaile ? Color.white : Color.red;
+    }
+}
