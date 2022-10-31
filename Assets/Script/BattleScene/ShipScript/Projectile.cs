598ceb63f3252ff9a458a38c14b183e0d4664eb4
jiho_kwon <imperiaiiist@naver.com>
2022-10-31 12:04:25 +0900

    전함 추가

--- Assets/Script/BattleScene/ShipScript/Projectile.cs
+++ Assets/Script/BattleScene/ShipScript/Projectile.cs
@@ -7,7 +7,7 @@ public class Projectile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        this.gameObject.SetActive(false);
     }
 
     float dmg;
