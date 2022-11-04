c947d859fd7f87abe018266c486a46a2b5e4623e
WikiMuncher <marimo3458@gmail.com>
2022-11-04 12:39:48 +0900

    광물 수집기 연출 코드 수정

--- Assets/Script/UI/Mover.cs
+++ Assets/Script/UI/Mover.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-
+    Vector3 posDest = Vector3.zero;
+    Vector3 posStart = Vector3.zero;
+    Vector3 dir = Vector3.zero;
     // Start is called before the first frame update
     public void Move(Vector3 pos)
     {
         // 약간의 오차
-        int randVal = 160;
+        int randVal = 20;
         Vector3 addpos = new Vector3(
             Random.Range(-randVal, randVal),
             Random.Range(-randVal, randVal)
@@ -17,16 +19,24 @@ public class Mover : MonoBehaviour
         transform.position += addpos;
 
         // 전진
-        Vector3 posDest = pos - transform.position;
-
-        GetComponent<Rigidbody2D>().AddForce(posDest.normalized * 1530, ForceMode2D.Impulse);
-        Destroy(gameObject, 0.5f);
-
+        posStart = transform.position;
+        posDest = pos;
+        dir = pos - posStart;
+        dir = dir.normalized;
+        Destroy(gameObject, 1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (posDest == Vector3.zero)
+            return;
+        var dist = Vector3.Distance(transform.position, posDest);
+        if (dist < 5.0f)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        transform.Translate(dir * 300 * Time.deltaTime, Space.Self);
     }
 }
