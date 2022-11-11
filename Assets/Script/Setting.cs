05ddedd3fdb14a469d5c1285f11c31dadbf5d3b2
pagi0721 <selly7213@gmail.com>
2022-11-11 11:59:11 +0900

    세팅창 첨부터 켜져있는거 수정

--- Assets/Script/Setting.cs
+++ Assets/Script/Setting.cs
@@ -7,6 +7,11 @@ public class Setting : MonoBehaviour
     // 원래는 sceneChange에 있었으나 부적절해 보여 이곳으로 옮기기로 했음
     public GameObject SettingWin;
 
+    private void Start()
+    {
+        SettingWin.SetActive(false);
+    }
+
     public void SettingsOn()
     {
         Time.timeScale = 0;
