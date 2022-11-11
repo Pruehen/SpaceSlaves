76e61283e97ceda8dcc5d5e4ea1742c0122bfd5a
pagi0721 <selly7213@gmail.com>
2022-11-11 16:28:22 +0900

    팁창 랜덤으로 바꿈

---
+++ Assets/Script/UI/TipView.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TipView : MonoBehaviour
+{
+    public List<string> TipTexts = new List<string>();
+    int StartText;
+    public TextMeshProUGUI TipText = null;
+
+    float MCooltime = 3f;
+    float DefaultMCooltime = 3f;
+
+    public void RandomPick()
+    {
+        StartText = Random.Range(0, TipTexts.Count);
+    }
+
+    public void TextViewNext()
+    {
+        TipText.text = TipTexts[StartText];      
+
+        if(StartText == TipTexts.Count - 1)
+        {
+            StartText = 0;
+        }
+        else
+        {
+            StartText++;
+        }
+    }
+
+    void Start()
+    {
+        RandomPick();
+        TextViewNext();
+    }
+
+    private void Update()
+    {
+        MCool();
+    }
+
+    void MCool()
+    {
+        MCooltime -= Time.deltaTime;
+
+        if (MCooltime <= 0)
+        {
+            MCooltime = DefaultMCooltime;
+            TextViewNext();
+        }
+    }
+}
